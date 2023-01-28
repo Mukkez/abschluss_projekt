8f667c9b947f470c5979bbf8bc78986b62b2ed5c
Mukkez <brick.freaky@brick-store.com>
2023-01-28 18:15:21 +0100

    update6

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -32,7 +32,7 @@ const MovieList = () => {
       } else if (genreId) {
          let modifiedGenreId = genreId.replace('&name', '');
          result = await defaultApi.getMoviesByGenre(modifiedGenreId, page);
-         setMovies([...movies, ...result.data.results.filter((movie) => movie.genre_ids[0] == modifiedGenreId)]);
+         setMovies([...movies, ...result.data.results.filter((movie) => movie.genre_ids[0] === modifiedGenreId)]);
       } else {
          result = await defaultApi.getMovies(page);
          setMovies([...movies, ...result.data.results]);
